71631be31256e27b82b12be7ab0838c670a7d0a9
Gordeev Arsenii <Gordeev.A.Vladim@sberbank.ru>
2020-10-04 23:11:58 +0300

    добавлены склоления блюд в корзине

--- app/views.py
+++ app/views.py
@@ -3,18 +3,37 @@ from flask import render_template, redirect, url_for, session
 from app import app
 from app.models import db, User, Dish, Category
 
+SESSION_KEY_OF_CART = 'cart'
+
+
+def get_cart_text():
+    dishes_in_cart = session.get(SESSION_KEY_OF_CART, [])
+    number_of_dishes = len(dishes_in_cart)
+    if not number_of_dishes:
+        return 'Корзина пуста'
+    sum = 0
+    for dish_in_cart in dishes_in_cart:
+        dish_in_cart_entity = db.session.query(Dish).get(dish_in_cart)
+        sum += dish_in_cart_entity.price
+    return f'В корзине {number_of_dishes} {decline_dish(number_of_dishes)} на сумму {sum} руб'
+
+
+def decline_dish(dish_number):
+    remainder = dish_number % 10
+    if remainder == 0 or remainder >= 5 or (10 <= dish_number <= 19):
+        return 'блюд'
+    elif remainder == 1:
+        return 'блюдо'
+    else:
+        return 'блюда'
+
 
 @app.route('/')
 def render_main():
     categories = db.session.query(Category).all()
     dishes = db.session.query(Dish).all()
-    dishes_in_cart = session.get('dishes', [])
-    sum = 0
-    for dish_in_cart in dishes_in_cart:
-        dish_in_cart_entity = db.session.query(Dish).get(dish_in_cart)
-        sum += dish_in_cart_entity.price
-
-    return render_template('main.html', categories=categories, dishes=dishes, dishes_in_cart=dishes_in_cart, sum=sum)
+    cart_text = get_cart_text()
+    return render_template('main.html', categories=categories, dishes=dishes, cart_text=cart_text)
 
 
 @app.route('/register')
@@ -44,7 +63,7 @@ def render_ordered():
 
 @app.route('/add_to_cart/<int:dish_id>')
 def add_to_cart(dish_id):
-    dishes = session.get('dishes', [])
+    dishes = session.get(SESSION_KEY_OF_CART, [])
     dishes.append(dish_id)
-    session['dishes'] = dishes
+    session[SESSION_KEY_OF_CART] = dishes
     return redirect(url_for('render_main'))
